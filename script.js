ce3cb67dcb4450c7e719f91243a7fe88589dd6ed
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-09-19 14:59:51 -0600

    Add arrays

---
+++ script.js
@@ -0,0 +1,15 @@
+var movQuotes = ["Jake Sully: All I ever wanted was a single thing worth fighting for.",
+"Trudy Chacon: You're not the only one with a gun,b***h.",
+"Jim Young: Anybody who tells you money is the root of all evil doesn't have any.",
+"Eames: You musn't be afraid to dream a little bigger, darling.",
+"Cobb: Never recreate from your memory. Always image new places!", "Alice: You're entirely bonkers. But I'll tell you a secret. All the best people are.",
+ "Rod Tidwell: Show me the money!",
+"Jimmy Dugan: It's supposed to be hard. If it wasn't hard, everyone would do it. The hard ... is what makes it great."];
+
+var movTitles = ["Avatar", "Avatar", "Boiler Room", "Inception", "Inception", "Alice in Wonderland", "Jerry Maguire", "A League of Their Own"];
+
+var movieQuote = getElementById("movie-quote");
+var movieTitle = getElementById("movie-title");
+
+movieQuote.innerText = messageText;
+movieTitle.innerText = messageText;
