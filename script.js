452e260c7becd3245bec15854989d48df936ac55
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-09-24 11:56:41 -0600

    Update font size for quotes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let movQuotes = ["Jake Sully: All I ever wanted was a single thing worth fighting for.",
-"Trudy Chacon: You're not the only one with a gun,b***h.",
+"Trudy Chacon: You're not the only one with a gun, b***h.",
 "Jim Young: Anybody who tells you money is the root of all evil doesn't have any.",
 "Eames: You musn't be afraid to dream a little bigger, darling.",
 "Cobb: Never recreate from your memory. Always image new places!", "Alice: You're entirely bonkers. But I'll tell you a secret. All the best people are.",
